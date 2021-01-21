833fd9644a21f9c28b497cf36d1634ad776a0876
tesfai05 <tesfai275@gmail.com>
2021-01-20 18:02:30 -0600

    Initial set up

---
+++ src/main/java/com/tesfai/jenkins/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.tesfai.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	
+	@GetMapping("/")
+	public String sampleMethod() {
+		return "Jenkins work fine";
+	}
+
+}
