235690667665521fd75517b237866cfa0b233dd0
Tesfai G <55055170+tesfai05@users.noreply.github.com>
2021-01-20 20:31:48 -0600

    Update SampleController.java

--- src/main/java/com/tesfai/jenkins/controller/SampleController.java
+++ src/main/java/com/tesfai/jenkins/controller/SampleController.java
@@ -4,16 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class SampleController {
 	
 	@GetMapping("/home")
 	public String sampleMethod() {
-		return "Jenkins is working fine";
+		return "Jenkins is working fine"
 	}
 	@GetMapping("/welcome/{name}")
 	public String welcomeMethod(@PathVariable String name) {
-		return name+", Welcome to Jenkins tutorial !";
+		return name+", Welcome to Jenkins tutorial !"
 	}
 
 }
