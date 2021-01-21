568ecb4901ddac1be8b267b1bb58632b812a359c
tesfai05 <tesfai275@gmail.com>
2021-01-20 19:23:25 -0600

    Add welcome method handler

--- src/main/java/com/tesfai/jenkins/controller/SampleController.java
+++ src/main/java/com/tesfai/jenkins/controller/SampleController.java
@@ -1,6 +1,7 @@
 package com.tesfai.jenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,10 @@ public class SampleController {
 	@GetMapping("/")
 	public String sampleMethod() {
 		return "Jenkins work fine";
+	}
+	@GetMapping("/{name}")
+	public String welcomeMethod(@PathVariable String name) {
+		return name+", Welcome brother to Jenkin tutorial !";
 	}
 
 }
